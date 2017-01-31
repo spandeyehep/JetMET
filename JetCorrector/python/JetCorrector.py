76633bdd5e3b25c381fa527fb57b84c49ad3c833
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2017-01-31 09:15:21 +0100

    improve download logic

--- JetCorrector/python/JetCorrector.py
+++ JetCorrector/python/JetCorrector.py
@@ -11,18 +11,6 @@ import ROOT
 import logging
 logger = logging.getLogger(__name__)
 
-# config
-correction_levels_data = [ 'L1FastJet', 'L2Relative', 'L3Absolute', 'L2L3Residual' ]
-Summer16_23Sep2016_DATA = \
-((1, 'Summer16_23Sep2016BCDV3_DATA'),
- (276831, 'Summer16_23Sep2016EFV3_DATA'),
- (278802, 'Summer16_23Sep2016GV3_DATA'),
- (280919, 'Summer16_23Sep2016HV3_DATA'))
-
-# config
-correction_levels_mc = [ 'L1FastJet', 'L2Relative', 'L3Absolute' ]
-Summer16_23Sep2016_MC = ((1, 'Summer16_23Sep2016V3_MC'),)
-
 def wget( source, target ):
     ''' Download source to target and make directories
     '''
@@ -30,6 +18,9 @@ def wget( source, target ):
         os.makedirs( os.path.dirname( target ) )
 
     urllib.urlretrieve( source, target )
+
+correction_levels_data = [ 'L1FastJet', 'L2Relative', 'L3Absolute', 'L2L3Residual' ]
+correction_levels_mc = [ 'L1FastJet', 'L2Relative', 'L3Absolute' ]
 
 class JetCorrector:
 
@@ -39,7 +30,6 @@ class JetCorrector:
             baseurl     = "https://github.com/cms-jet/JECDatabase/raw/master/tarballs/",
             jetFlavour  = 'AK4PFchs',
             directory   = "$CMSSW_BASE/src/JetMET/JetCorrector/data/",
-            overwrite   = False,
             ):
 
         extension   = "tar.gz"
@@ -48,28 +38,34 @@ class JetCorrector:
             # Download file
             source = os.path.join( baseurl, "%s.%s"%(filename, extension) )
             target = os.path.join( os.path.expandvars( directory ), "%s.%s"%(filename, extension) )
-            if not os.path.exists( target ) or overwrite:
-                logger.info( "Downloading %s to %s.", source, target )
-                wget( source, target )
-                # Extract txt files
-                with tarfile.open(target, 'r:gz') as tar:
-                    for member in tar.getmembers():
-                        if not member.name.endswith( jetFlavour+'.txt' ): continue
-                        logger.debug( "Found file %s in %s", member.name, target )
-                        with file( os.path.join( os.path.expandvars(directory), member.name), 'w') as f_out:
-                            f_out.writelines( tar.extractfile( member ).readlines() )
-            else:
-                logger.info( "Found %s.", target )
 
-            # Make corrector
-            self.vPar = ROOT.vector(ROOT.JetCorrectorParameters)()
+            params = ROOT.vector(ROOT.JetCorrectorParameters)()
+            
             for level in correctionLevels:
                 txtfile = os.path.join( os.path.expandvars(directory), "%s_%s_%s.txt"%( filename, level, jetFlavour)  )
-                self.vPar.push_back( ROOT.JetCorrectorParameters( txtfile, "" ) )
+                # Do we have the txt file?
+                if not os.path.exists( txtfile ):
+                    logger.info( "txt file %s not found.", txtfile )
+                    # Do we actually have the targ.gz?
+                    if not os.path.exists( target ):
+                        logger.info( "%s not found. Downloading from %s.", target, source )
+                        wget( source, target )
 
-            self.jetCorrectors.append( ( runnumber,  ROOT.FactorizedJetCorrector(self.vPar)) )
+                    # Extract txt files
+                    logger.info( "Extracting %s", target )
+                    with tarfile.open(target, 'r:gz') as tar:
+                        for member in tar.getmembers():
+                            if not member.name.endswith( jetFlavour+'.txt' ): continue
+                            logger.debug( "Found file %s in %s", member.name, target )
+                            with file( os.path.join( os.path.expandvars(directory), member.name), 'w') as f_out:
+                                f_out.writelines( tar.extractfile( member ).readlines() )
 
-        # Sort IOVs
+                params.push_back( ROOT.JetCorrectorParameters( txtfile, "" ) )
+
+            # Make corrector
+            self.jetCorrectors.append( ( runnumber,  ROOT.FactorizedJetCorrector( params )) )
+
+        # Sort wrt IOVs
         self.jetCorrectors.sort( key = lambda p: p[0] )
 
     def correction(self, rawPt, eta, area, rho, run = 1):
@@ -81,12 +77,3 @@ class JetCorrector:
                 corrector.setJetA( area )
                 corrector.setRho( rho )
                 return corrector.getCorrection()
-
-if __name__ == "__main__":
-
-    # Logging
-    import JetMET.tools.logger as logger
-    logger  = logger.get_logger('INFO', logFile = None)
-
-    jetCorrector_data = JetCorrector( Summer16_23Sep2016_DATA, correctionLevels = correction_levels_data ) 
-    jetCorrector_mc   = JetCorrector( Summer16_23Sep2016_MC, correctionLevels = correction_levels_mc ) 
